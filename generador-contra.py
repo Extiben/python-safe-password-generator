cb83758f5d676e17542d1bab5bf47a0991f8da98
Extiben <jhon.mosquerapalacios@inemjose.edu.co>
2025-07-10 11:27:30 -0500

    first commit

---
+++ generador-contra.py
@@ -0,0 +1,8 @@
+import random
+import string 
+
+longitud = 18
+
+caracteres = string.ascii_letters + string.digits + string.punctuation
+password = ''.join(random.choice(caracteres) for _ in range (longitud))
+print("nueva contraseña segura: ", password)
